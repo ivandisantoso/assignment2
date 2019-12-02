86e7a209c0f119ef198146990083a3dd8722b373
Ivandi Santoso <ivandisantoso@Vahns-MacBook-Pro.local>
2019-12-02 14:01:04 -0500

    Calculation for regular worker has done

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@
   // let faculty = document.getElementById(`facultyRdo`);
   // let magister = document.getElementById(`masterRdo`);
   // let bachelor = document.getElementById(`bachelorRdo`);
-  const regularWorkingHourMonthly = 160;
+  const regularMinWorkHourMonthly = 160;
   const regularAllowance = 2500;
   const masterSalaryPerHour = 175;
   const bachelorSalaryPerHour = 100;
@@ -36,11 +36,8 @@ function calculateWorkDay(){
   let numberWorkDays = subDayEnd-subDayStart;
   let numberWorkMonths = subMonthEnd-subMonthStart;
   let numberWorkYears = subYearEnd-subYearStart;
-  let test1 = new Date(startWork);
-  let test2 = new Date(endWork);
-  let test11 = test1.ge();
+ 
 // console.log(`Working Days ${subYearStart} working days ${subYearEnd}`);
-console.log(`typeof ${test11}`)
   // if (test1>test2){
   //   console.log(`>`);
   // } else {
@@ -48,129 +45,134 @@ console.log(`typeof ${test11}`)
   // }
 }
 
-  function isRegularFacultyChecked(){
-    let regular = document.getElementById(`regularRdo`);
-    // let faculty = document.getElementById(`facultyRdo`);
+function isRegularFacultyChecked(){
+  let regular = document.getElementById(`regularRdo`);
+  // let faculty = document.getElementById(`facultyRdo`);
 
-    if (regular.checked == true){
-      console.log(`Regular is checked `);
-    }else{
-      console.log(`Faculty is checked `);
-    }
+  if (regular.checked == true){
+    console.log(`Regular is checked `);
+  }else{
+    console.log(`Faculty is checked `);
   }
+}
 
-  const domRadios = document.querySelectorAll('[name="radioOnOrOff"]')
-  domRadios.forEach(radioBtn => {
+const domRadios = document.querySelectorAll('[name="radioOnOrOff"]')
+domRadios.forEach(radioBtn => {
+  radioBtn.addEventListener('change', event => {
+    
+    // console.log(event.target.value);
+    if (event.target.value == "1") {
+      renderMasterBachelorRadio();
+    } else {
+      masterBachelor.innerHTML =
+        `<fieldset id="macBacFld" hidden>
+    <ul>
+      <li>
+        <label class="option">
+          <input type="radio" name="macBacRdo" value="1" id="masterRdo" class="radio" checked> 
+          <span class="faculty">Master</span>
+        </label>                  
+        <div class="option">
+          <input type="radio" name="macBacRdo" value="0" id="bachelorRdo" class="radio">
+          <label class="radio-btn">Bachelor</label>
+        </div>
+      </li>
+    </ul>
+  </fieldset>`;
+    }
+  })
+})
+
+function renderMasterBachelorRadio(){
+  masterBachelor.innerHTML =
+    `<fieldset id="macBacFld">
+  <label class="option">
+      <input type="radio" name="radioOnOrOff1" value="0" id="bachelorRdo" class="radio">
+      <label for="bachelorRdo" class="radio-btn">Bachelor</label>
+  </label>                  
+  <label class="option">
+    <input type="radio" name="radioOnOrOff1" value="1" id="masterRdo" class="radio"> 
+    <span class="radio-btn">Master</span>
+  </label></fieldset>
+`;
+  const domRadios1 = document.querySelectorAll('[name="radioOnOrOff1"]')
+  domRadios1.forEach(radioBtn => {
     radioBtn.addEventListener('change', event => {
-      
-      // console.log(event.target.value);
-      if (event.target.value == "1") {
-        renderMasterBachelorRadio();
+      console.log(event.target.value);
+      if (event.target.value == "0") {
+      alert(`Bachelor is checked`);
+
       } else {
-        masterBachelor.innerHTML =
-          `<fieldset id="macBacFld" hidden>
-      <ul>
-        <li>
-          <label class="option">
-            <input type="radio" name="macBacRdo" value="1" id="masterRdo" class="radio" checked> 
-            <span class="faculty">Master</span>
-          </label>                  
-          <div class="option">
-            <input type="radio" name="macBacRdo" value="0" id="bachelorRdo" class="radio">
-            <label class="radio-btn">Bachelor</label>
-          </div>
-        </li>
-      </ul>
-    </fieldset>`;
+  
+        alert(`Master is checked`);
+
       }
     })
   })
 
-  function renderMasterBachelorRadio(){
-    masterBachelor.innerHTML =
-      `<fieldset id="macBacFld">
-    <label class="option">
-        <input type="radio" name="radioOnOrOff1" value="0" id="bachelorRdo" class="radio">
-        <label for="bachelorRdo" class="radio-btn">Bachelor</label>
-    </label>                  
-    <label class="option">
-      <input type="radio" name="radioOnOrOff1" value="1" id="masterRdo" class="radio"> 
-      <span class="radio-btn">Master</span>
-    </label></fieldset>
-  `;
-    const domRadios1 = document.querySelectorAll('[name="radioOnOrOff1"]')
-    domRadios1.forEach(radioBtn => {
-      radioBtn.addEventListener('change', event => {
-        console.log(event.target.value);
-        if (event.target.value == "0") {
-        alert(`Bachelor is checked`);
+}
 
-        } else {
-    
-          alert(`Master is checked`);
 
-        }
-      })
-    })
-  
+function calculateSalary(){
+  let result = document.getElementById(`resultArt`);
+  let number = document.getElementById(`numberTxt`).value;
+  let name = document.getElementById(`nameTxt`).value;
+  let department = document.getElementById(`departmentSlct`).value;
+  // let hours = document.getElementById(`hoursTxt`).value;
+  let month = document.getElementById(`monthSlct`).value;
+  // let regular = document.getElementById(`regularRdo`);
+  // let faculty = document.getElementById(`facultyRdo`);
+
+  isRegularFacultyChecked();
+  // const domRadios = document.querySelectorAll('[name="radioOnOrOff"]');
+
+
+
+result.innerHTML = `Number :${number} Name :${name} Dept :${department} Hours ${hours} Month :${month} `;
+// document.getElementById(`resultArt`).innerHTML = number;
+
+
+//alert(`numb ${number.value}`);
+
+}
+
+function calculateRegularProrated(){
+  let regularProrated = regularAllowance / regularMinWorkHourMonthly;
+  // console.log(`Hourly Rate ${regularProrated}`); $15.625
+  return regularProrated;
+}
+
+function calculateRegularSalary(){
+  let hours = document.getElementById(`hoursTxt`).value;
+  let regularProrated = calculateRegularProrated();
+  let regularGrossSalary = (hours * regularProrated);
+  let regularNetSalary = regularGrossSalary-(regularGrossSalary * incomeTax);
+  let doubleProratedRegularGrossSalary = (hours-regularMinWorkHourMonthly)*(regularProrated * 1)+regularGrossSalary; 
+  let doubleProratedRegularNetSalary = doubleProratedRegularGrossSalary-(doubleProratedRegularGrossSalary * incomeTax);
+
+  if(hours <= regularMinWorkHourMonthly){
+    console.log(` Gross Salary${regularGrossSalary.toFixed(2)} Net Salary ${regularNetSalary.toFixed(2)}`);
+  }else { 
+    console.log(` Gross Salary double prorated ${doubleProratedRegularGrossSalary.toFixed(2)} Net Salary double prorated ${doubleProratedRegularNetSalary.toFixed(2)}`);
   }
+}
+
+function calculateFacultyMasterGrossSalary(){
+  let hours = document.getElementById(`hoursTxt`).value; 
+  let facultyMasterGrossSalary = masterAllowance + (masterSalaryPerHour * hours);
+ 
+  console.log(facultyMasterGrossSalary);
+}
+
+function calculateFacultyBachelorGrossSalary(){
+  let hours = document.getElementById(`hoursTxt`).value; 
+  let facultyBachelorGrossSalary = bachelorAllowance + (bachelorSalaryPerHour * hours);
+  console.log(facultyBachelorGrossSalary);
+}
 
 
-  function calculateSalary(){
-    let result = document.getElementById(`resultArt`);
-    let number = document.getElementById(`numberTxt`).value;
-    let name = document.getElementById(`nameTxt`).value;
-    let department = document.getElementById(`departmentSlct`).value;
-    // let hours = document.getElementById(`hoursTxt`).value;
-    let month = document.getElementById(`monthSlct`).value;
-    // let regular = document.getElementById(`regularRdo`);
-    // let faculty = document.getElementById(`facultyRdo`);
-
-    isRegularFacultyChecked();
-    // const domRadios = document.querySelectorAll('[name="radioOnOrOff"]');
-  
 
 
-  result.innerHTML = `Number :${number} Name :${name} Dept :${department} Hours ${hours} Month :${month} `;
-  // document.getElementById(`resultArt`).innerHTML = number;
 
-  
-  //alert(`numb ${number.value}`);
-
-  }
-  function getHourlyRate(){
-
-  }
-
-  function calculateFacultyMasterGrossSalary(){
-    let hours = document.getElementById(`hoursTxt`).value; 
-    let facultyMasterGrossSalary = masterAllowance + (masterSalaryPerHour * hours);
-    console.log(facultyMasterGrossSalary);
-  }
-
-  function calculateFacultyBachelorGrossSalary(){
-    let hours = document.getElementById(`hoursTxt`).value; 
-    let facultyBachelorGrossSalary = bachelorAllowance + (bachelorSalaryPerHour * hours);
-    console.log(facultyBachelorGrossSalary);
-  }
-
-  function calculateRegularGrossSalary(){
-    let hours = document.getElementById(`hoursTxt`).value;
-
-  }
-
-  function calculateTax(){
-    let hours = document.getElementById(`hoursTxt`).value;
-    let salaryTax = regularGrossSalaryHour-(regularGrossSalaryHour * incomeTax);  
-    console.log (salaryTax);
-  }
-
-  function calculateSalaryHour(){
-    // let hours = document.getElementById(`hoursTxt`).value;
-    // calculateTax();
-    // let salaryHour = hours*salaryTax;
-    //console.log()
-  }
-
-  // EXE
-  calculate.addEventListener("click",calculateWorkDay);
+// EXE
+calculate.addEventListener("click",calculateRegularSalary);
